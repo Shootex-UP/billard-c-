663adc443e99c9b5e7ee16f867aa8a752800015e
JeanDev <hardyjea@gmail.com>
2019-10-07 11:30:59 +0200

    Added pollevent to close window

--- billard-c++/billard-c++.cpp
+++ billard-c++/billard-c++.cpp
@@ -12,11 +12,19 @@ int main()
 	sf::RenderWindow window(sf::VideoMode(WINDOWS_WIDTH, WINDOWS_HEIGHT), "Billard C++");
     std::cout << "Hello World!\n"; 
 
+	sf::Event event;
+
 	sf::Clock clock;
 	float frameDelay = 1.f / FRAMERATE;//sec
 	float deltaTime = 0;
 	while (window.isOpen())
 	{
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
 		deltaTime = clock.getElapsedTime().asSeconds();
 		if (deltaTime >= frameDelay) {
 			//std::cout << deltaTime << "sec" << std::endl;
