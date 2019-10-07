03768457e1da68926caa4e0ac28241decc23cd93
mathieuway91 <mathieu.way91@gmail.com>
2019-10-07 10:46:19 +0200

    SFML setup

--- billard-c++/billard-c++.cpp
+++ billard-c++/billard-c++.cpp
@@ -1,21 +1,30 @@
-// billard-c++.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <SFML/Graphics.hpp>
+
+#define WINDOWS_WIDTH 1920
+#define WINDOWS_HEIGHT 960
+#define FRAMERATE 60
 
 int main()
 {
+
+	sf::RenderWindow window(sf::VideoMode(WINDOWS_WIDTH, WINDOWS_HEIGHT), "Billard C++");
     std::cout << "Hello World!\n"; 
+
+	sf::Clock clock;
+	float frameDelay = 1.f / FRAMERATE;//sec
+	float deltaTime = 0;
+	while (window.isOpen())
+	{
+		deltaTime = clock.getElapsedTime().asSeconds();
+		if (deltaTime >= frameDelay) {
+			//std::cout << deltaTime << "sec" << std::endl;
+			window.clear();
+			//draw
+			window.display();
+			clock.restart();
+		}
+	}
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
