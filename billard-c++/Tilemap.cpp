5df07c98c90d8da3b6b28c6e8a560c4830d77612
JeanDev <hardyjea@gmail.com>
2019-10-09 10:40:18 +0200

    Création fichiers Tilemap .cpp et .h

---
+++ billard-c++/Tilemap.cpp
@@ -0,0 +1,12 @@
+#include "pch.h"
+#include "Tilemap.h"
+
+
+Tilemap::Tilemap()
+{
+}
+
+
+Tilemap::~Tilemap()
+{
+}
---
+++ billard-c++/Tilemap.h
@@ -0,0 +1,8 @@
+#pragma once
+class Tilemap
+{
+public:
+	Tilemap();
+	~Tilemap();
+};
+
