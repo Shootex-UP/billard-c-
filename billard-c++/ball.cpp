466ab0db126a87c2fb1fe54461e4270fc9af0efe
paul thibault <paulthb1@gmail.com>
2019-10-09 12:02:33 +0200

    update ball script

--- billard-c++/ball.cpp
+++ billard-c++/ball.cpp
@@ -1,14 +1,21 @@
 #include "pch.h"
 #include "ball.h"
-#include "TileMap.h"
 
 
-ball::ball()
+ball::ball(std::string tileMapPath, std::string idBall): _idBall(idBall)
 {
-
+	_windowBall = new sf::RenderWindow(sf::VideoMode(_winwidth, _winHeight), _idBall, sf::Style::None);
+	_tileMap = new Tilemap(tileMapPath, 48, 24, 16, 16);
 }
 
 
 ball::~ball()
 {
+}
+
+void ball::DrawBall()
+{
+	_windowBall->clear();
+	_tileMap->DrawTileMap(&_mainSpriteSheet);
+	_windowBall->display();
 }
--- billard-c++/ball.h
+++ billard-c++/ball.h
@@ -1,8 +1,29 @@
 #pragma once
+#include "Spritesheet.h"
+#include "TileMap.h"
+#include <string>
+#include <SFML/Graphics.hpp>
+
+
 class ball
 {
+	static Spritesheet _mainSpriteSheet;
+	static float _winHeight;
+	static float _winwidth;
+
+	std::string _idBall;
+
+	float _winPosX;
+	float _winPosY;
+
+	Tilemap* _tileMap;
+	sf::RenderWindow* _windowBall;
+
+
 public:
-	ball();
+	ball(std::string tileMapPath , std::string idBall);
 	~ball();
+
+	void DrawBall();
 };
 
